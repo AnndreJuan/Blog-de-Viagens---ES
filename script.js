0e525b56ee4f7eb1437f39eaa6c98b14ceba020b
AnndreJuan <anndret26@gmail.com>
2024-05-23 19:36:08 -0300

    revisao1

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function headerBlur() {
     if(window.scrollY > 70){
         header.style.background = 'linear-gradient(40deg, #2c555c 0%, #44203c 70%)';
         header.style.boxShadow = '0px 0px 10px #1a191e';
-        console.log('FOi');
     } else {
         header.style.background = '';
         header.style.boxShadow = '';
